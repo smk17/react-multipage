869f8e1bd2a6e6d1b2dc54aa60f5ada2e8412da9
1602228514 <1602228514@qq.com>
2018-08-02 22:29:06 +0800

    增加应用列表

---
+++ src/AppStore/Middleground/Application/App.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { DingTalk } from '@/common/DingTalk';
+import YdyScrollView from "@/components/YdyScrollView";
+import loading from '@/assets/img/load.gif';
+import './App.less';
+import { Card, List } from 'antd';
+import Ellipsis from 'ant-design-pro/lib/Ellipsis';
+
+class App extends React.Component<any, AppStateTypes> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      load: false,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  }
+
+  componentDidMount () {
+    DingTalk.init().then(() => {
+      this.setState({
+        load: true,
+      })
+    })
+  }
+  
+  renderContent () {
+
+    const { list } = this.props;
+    return (
+      <YdyScrollView>   
+        <div className="cardList">
+          <List
+            rowKey="id"
+            grid={{ gutter: 24, lg: 3, md: 2, sm: 1, xs: 1 }}
+            dataSource={list}
+            renderItem={item =>
+              (
+                <List.Item key={item.id}>
+                  <Card hoverable className="card" actions={[<a>申请</a>, <a>续费</a>]}>
+                    <Card.Meta
+                      avatar={<img alt="" className="cardAvatar" src={item.avatar} />}
+                      title={<a href="#">{item.title}</a>}
+                      description={
+                        <Ellipsis className="item" lines={3}>
+                          {item.description}
+                        </Ellipsis>
+                      }
+                    />
+                  </Card>
+                </List.Item>
+              )
+            }
+          />
+        </div>
+      </YdyScrollView>
+    );
+  }
+  
+  render() {
+    const LoadView = (
+      <img className="App-loading" src={loading} alt="" />
+    );
+    return (
+      <div className="App" style={{ width: this.state.width + 'px', height: this.state.height + 'px' }}>
+        {
+          this.state.load ? this.renderContent() : LoadView
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/AppStore/Middleground/Application/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import axios from 'axios';
+import 'es6-set/implement';
+import 'es6-map/implement';
+import ReactDOM from 'react-dom';
+import './index.less';
+import App from './App';
+import { Service } from "@/common/Service";
+import BetterJs from "@/common/BetterJs";
+import YdyScrollView from "@/components/YdyScrollView";
+import registerServiceWorker from '@/registerServiceWorker';
+
+window.baseConfig = {
+  "host": "http://192.168.0.188:8003",
+  "development": false
+}
+axios.get('/config.json').then(res => {
+  window.baseConfig = res.data;
+  if (!window.baseConfig.development) {
+    BetterJs.init({
+      sendError: (error) => {
+        Service.writeLog(JSON.stringify(error))
+      }
+    })
+  }
+  ReactDOM.render((<YdyScrollView><App /></YdyScrollView>), document.getElementById('root'));
+})
+registerServiceWorker();
