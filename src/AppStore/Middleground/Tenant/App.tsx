b56aef022e2b959ce896cf10fb4f15b5be37b6f1
1602228514 <1602228514@qq.com>
2018-08-02 16:47:37 +0800

    修改租户管理页面

--- src/AppStore/Middleground/Tenant/App.tsx
+++ src/AppStore/Middleground/Tenant/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import YdyScrollView from "@/components/YdyScrollView";
 import loading from '@/assets/img/load.gif';
+import { Form,Input,Card,} from 'antd';
+import { createForm } from 'rc-form';
 import './App.less';
+
+const FormItem = Form.Item;
 
 class App extends React.Component<any, AppStateTypes> {
   constructor(props) {
@@ -20,8 +24,94 @@ class App extends React.Component<any, AppStateTypes> {
   }
   
   renderContent () {
+
+    const { getFieldDecorator } = this.props.form;
+
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 7 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 12 },
+        md: { span: 10 },
+      },
+    };
+
     return (
-      <YdyScrollView>新的页面从这里开始吧！</YdyScrollView>
+      <YdyScrollView>   
+        <Card bordered={false}>
+          <Form>
+          <FormItem {...formItemLayout} label="公司名称">
+              {getFieldDecorator('tenantname', {
+                rules: [
+                  {
+                    required: true,
+                    message: '公司名称不能为空',
+                  },
+                ],
+              })(<Input placeholder="请输入公司名称" />)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="公司名称">
+              {getFieldDecorator('code', {
+                rules: [
+                  {
+                    required: true,
+                    message: '登陆名不能为空',
+                  },
+                ],
+              })(<Input placeholder="请输入登陆名" />)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="密码">
+              {getFieldDecorator('password', {
+                rules: [
+                  {
+                    required: true,
+                    message: '密码不能为空',
+                  },
+                ],
+              })(<Input type="password" placeholder="请输入密码" />)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="姓名">
+              {getFieldDecorator('name', {
+                rules: [
+                  {
+                    required: true,
+                    message: '姓名不能为空',
+                  },
+                ],
+              })(<Input placeholder="请输入姓名" />)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="邮箱">
+              {getFieldDecorator('email', {
+                rules: [
+                  {
+                    type: 'email', message: '邮箱格式不正确',
+                  },
+                  {
+                    required: true,
+                    message: '邮箱不能为空',
+                  },
+                ],
+              })(<Input placeholder="请输入邮箱" />)}
+            </FormItem>
+            <FormItem {...formItemLayout} label="手机">
+              {getFieldDecorator('mobile', {
+                rules: [
+                  {
+                    pattern: /^-?(0|[1-9][0-9]*)(\.[0-9]*)?$/, message: '手机格式不正确',
+                  },
+                  {
+                    required: true,
+                    message: '手机不能为空',
+                  },
+                ],
+              })(<Input addonBefore="+86" placeholder="请输入手机" />)}
+            </FormItem>
+          </Form>
+        </Card>
+      </YdyScrollView>
     );
   }
   
@@ -39,4 +129,4 @@ class App extends React.Component<any, AppStateTypes> {
   }
 }
 
-export default App;
+export default  createForm()(App);
